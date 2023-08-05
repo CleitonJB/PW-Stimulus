7a7c8e53222e06d6fb1f9bceeb5160fa4736dd48
Cleiton Braga <cleitonbraga56@gmail.com>
2023-08-04 23:58:56 -0300

    Set 'UserControllerView' routes to REST format

--- Stimulus/src/main/java/com/stimulus/main/controller/view/UserControllerView.java
+++ Stimulus/src/main/java/com/stimulus/main/controller/view/UserControllerView.java
@@ -2,27 +2,18 @@ package com.stimulus.main.controller.view;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.stimulus.main.entities.User;
-
 @Controller
-@RequestMapping(path = "/user")
+@RequestMapping(path = "/form")
 public class UserControllerView {
-	@GetMapping("/form/new")
-	public String addUser() {
-		return "addUser";
+	@GetMapping("/users")
+	public String listUsers() {
+		return "listUsers";
 	}
 	
-	@PostMapping("/form/new2")
-	public void addUser2(@RequestBody User user) {
-		System.out.println("USER 2: " + user);
+	@GetMapping("/user")
+	public String formUser() {
+		return "formUser";
 	}
-	
-//	@PostMapping("/form/new3")
-//	public void addUser3(@ModelAttribute User user) {
-//		System.out.println("USER 3: " + user);
-//	}
 }
