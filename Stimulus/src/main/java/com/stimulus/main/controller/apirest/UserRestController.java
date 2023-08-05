8faa6973eb741239a8d14f84a24cc514da1d956d
Cleiton Braga <cleitonbraga56@gmail.com>
2023-08-04 23:58:29 -0300

    Set 'UserRestController' routes to REST format

--- Stimulus/src/main/java/com/stimulus/main/controller/apirest/UserRestController.java
+++ Stimulus/src/main/java/com/stimulus/main/controller/apirest/UserRestController.java
@@ -1,7 +1,6 @@
 package com.stimulus.main.controller.apirest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,18 +15,9 @@ public class UserRestController {
 	@Autowired
 	private UserService userService;
 	
-	@PostMapping(value = "/new")//, consumes={"application/x-www-form-urlencoded"}
-	public String addUser(@RequestBody User user) {
-		System.out.println("USER 11: " + user);
-//		User u = userService.save(user, null);
-		userService.save(user, null);
-		return "Usuário cadastrado com sucesso: " + user.getId(); // model.toString()
-	}
-	
-	@PostMapping(value = "/new2")//, consumes={"application/x-www-form-urlencoded"}
-	public String addUser2(@ModelAttribute User user) {
-		System.out.println("USER 22: " + user);
-//		User u = userService.save(user, null);
+	@PostMapping(value = "")
+	public String create(@RequestBody User user) {
+		System.out.println("USER: " + user);
 		userService.save(user, null);
 		return "Usuário cadastrado com sucesso: " + user.getId(); // model.toString()
 	}
