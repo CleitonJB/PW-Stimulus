58efb29a9c67e7cf9bbf107298913e9c8b575b32
Cleiton Braga <cleitonbraga56@gmail.com>
2023-08-11 00:38:57 -0300

    Recreate logic to 'Comment' and 'Post' class with 'Publication' superclass

---
+++ Stimulus/src/main/java/com/stimulus/main/entities/Publication.java
@@ -0,0 +1,43 @@
+package com.stimulus.main.entities;
+
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class Publication {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@Enumerated
+	private PublicationTypes type;
+	private int likes;
+	private int comments;
+	
+	public Long getId() {
+		return Long.valueOf(this.id);
+	}
+	
+	public PublicationTypes getType() {
+		return this.type;
+	}
+	public void setType(PublicationTypes type) {
+		this.type = type;
+	}
+	
+	public int getLikes() {
+		return this.likes;
+	}
+	public void setLikes(int likes) {
+		this.likes = likes;
+	}
+	
+	public int getComments() {
+		return this.comments;
+	}
+	public void setComments(int comments) {
+		this.comments = comments;
+	}
+}
