32eafe3e31f77a9c1990a88d1a123533389f355a
Cleiton Braga <cleitonbraga56@gmail.com>
2023-08-17 17:40:04 -0300

    Add try catch logic to Controller of 'Comment' And check if 'User' exist when save

--- Stimulus/src/main/java/com/stimulus/main/controller/apirest/CommentRestController.java
+++ Stimulus/src/main/java/com/stimulus/main/controller/apirest/CommentRestController.java
@@ -28,33 +28,57 @@ public class CommentRestController {
 	@Operation(summary = "Obter uma lista de comentários", description = "Obter uma lista dos comentários cadastrados", tags = { "Comment" })
 	@GetMapping(value = "/comments")
 	public ResponseEntity getAll() {
-		return ResponseEntity.ok(commentService.findAll());
+		try {
+			return ResponseEntity.ok(commentService.findAll());
+		} catch(Exception error) {
+			return ResponseEntity.status(500).body(error.getMessage());
+		}
 	}
 
 	@Operation(summary = "Obter um comentário", description = "Obter um comentário específico baseado no seu campo 'id'", tags = { "Comment" })
 	@GetMapping(value = "/comment/{id}")
 	public ResponseEntity getOne(@PathVariable("id") Long id) {
-		return ResponseEntity.ok(commentService.findById(id));
+		try {
+			return ResponseEntity.ok(commentService.findById(id));
+		} catch(Exception error) {
+			return ResponseEntity.status(500).body(error.getMessage());
+		}
 	}
 
 	@Operation(summary = "Criar um comentário", description = "Criar um novo comentário (caso já não exista)", tags = { "Comment" })
 	@PostMapping(value = "/comment")
 	public ResponseEntity save(@RequestBody Comment comment) {
-		commentService.save(comment);
-		return ResponseEntity.status(HttpStatus.CREATED).body(comment);
+		try {
+			if(comment.getUser() == null) {
+				return ResponseEntity.badRequest().body("O Usário não foi informado!");
+			} else {
+				commentService.save(comment);
+				return ResponseEntity.status(HttpStatus.CREATED).body(comment);
+			}
+		} catch(Exception error) {
+			return ResponseEntity.status(500).body(error.getMessage());
+		}
 	}
 
 	@Operation(summary = "Atualizar os dados de um comentário", description = "Atualizar os dados de um comentário (enviar todos os campos)", tags = { "Comment" })
 	@PutMapping(value = "/comment")
 	public ResponseEntity update(@RequestBody Comment comment) {
-		commentService.update(comment);
-		return ResponseEntity.status(HttpStatus.OK).build();
+		try {
+			commentService.update(comment);
+			return ResponseEntity.status(HttpStatus.OK).build();
+		} catch(Exception error) {
+			return ResponseEntity.status(500).body(error.getMessage());
+		}
 	}
 
 	@Operation(summary = "Excluir um comentário", description = "Excluir um comentário baseado no seu campo 'id'", tags = { "Comment" })
 	@DeleteMapping(value = "/comment/{id}")
 	public ResponseEntity delete(@PathVariable("id") Long id) {
-		commentService.deleteById(id);
-		return ResponseEntity.ok().build();
+		try {
+			commentService.deleteById(id);
+			return ResponseEntity.ok().build();
+		} catch(Exception error) {
+			return ResponseEntity.status(500).body(error.getMessage());
+		}
 	}
 }
