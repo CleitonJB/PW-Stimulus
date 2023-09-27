4ca56d74e370a0268929a71a08906302b3bad6ac
Cleiton Braga <cleitonbraga56@gmail.com>
2023-09-27 02:23:10 -0300

    Insert 'date' field to 'Publication' class entitie

--- Stimulus/src/main/java/com/stimulus/main/entities/Publication.java
+++ Stimulus/src/main/java/com/stimulus/main/entities/Publication.java
@@ -1,5 +1,8 @@
 package com.stimulus.main.entities;
 
+import java.sql.Timestamp;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -7,6 +10,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import jakarta.validation.constraints.NotNull;
 
 @MappedSuperclass
@@ -14,6 +19,10 @@ public class Publication {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+	@NotNull
+	@Column(name="date", insertable=false, updatable=false, columnDefinition="DATETIME")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Timestamp date;
 	@NotNull
 	@ManyToOne
     @JoinColumn(name = "user_id")
@@ -29,6 +38,13 @@ public class Publication {
 //	public void setId(Long id) {
 //		this.id = id;
 //	}
+	
+	public Timestamp getDate() {
+		return this.date;
+	}
+	public void setDate(Timestamp date) {
+		this.date = date;
+	}
 	
 	public User getUser() {
 		return this.user;
